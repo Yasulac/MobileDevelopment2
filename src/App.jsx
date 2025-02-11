e33a5a0060b5ebaaad437915030c2320731cf945
EB206-U9 <lcalusay@ssct.edu.ph>
2025-02-11 12:05:46 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+function App(){
+
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  return (
+  <div>
+  <h1>Number :{count} </h1>
+  <button onClick={increment}>Increment</button>
+  </div>
+  )
+}
+
+export default App;
+
